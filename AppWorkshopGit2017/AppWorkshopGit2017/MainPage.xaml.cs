24b01a4ff7dbed423f950b0514d090c85515bebb
Cesar Morales <cmorales@thedataage.com>
2017-11-01 10:08:48 -0600

    En el acceso a la aplicación que persista el email

--- AppWorkshopGit2017/AppWorkshopGit2017/MainPage.xaml.cs
+++ AppWorkshopGit2017/AppWorkshopGit2017/MainPage.xaml.cs
@@ -14,19 +14,46 @@ namespace AppWorkshopGit2017
         {
             InitializeComponent();
         }
-        
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            if (Application.Current.Properties.ContainsKey("email"))
+            {
+                string emailGuardado = Application.Current.Properties["email"] as string;
+                if (!string.IsNullOrWhiteSpace(emailGuardado))
+                {
+                    EntryEmail.Text = emailGuardado;
+                    //Enviar();
+                }
+            }
+        }
+
         private void ButtonEnviar_Clicked(object sender, EventArgs e)
         {
-            var email = EntryEmail.Text;
-            if (!string.IsNullOrEmpty(email))
+            Enviar();
+        }
+
+        private async void Enviar()
+        {
+            string email = EntryEmail.Text;
+
+            if (string.IsNullOrEmpty(email))
             {
-                Intent intent = new Intent(this, new NavigationPage( new MainTabbedPage()));
-                intent.StartIntent();
+                var respuesta = await DisplayAlert("Error", "No ha escrito un email válido", "Omitir", "Volver");
+                if (!respuesta)
+                    return;
             }
+
+            // Persiste el dato del email en la aplicación
+            if (Application.Current.Properties.ContainsKey("email"))
+                Application.Current.Properties["email"] = email;
             else
-            {
-                DisplayAlert("Error","Escriba un email válido", "Aceptar");
-            }
+                Application.Current.Properties.Add("email", email);
+
+            Intent intent = new Intent(this, new NavigationPage(new MainTabbedPage()));
+            intent.StartIntent();
         }
     }
 }
